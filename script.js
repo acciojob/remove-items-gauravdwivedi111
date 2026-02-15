e4f274976b5289614425fc596f953eda3aa54b0f
gauravdwivedi111 <dwivedigaurav646@gmail.com>
2026-02-15 17:06:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+	const button = document.querySelector('input[type="button"]');
+
+	button.addEventListener("click", function () {
+
+        const select = document.getElementById("colorSelect");
+
+        const selectedIndex = select.selectedIndex;
+
+        if (selectedIndex !== -1) {
+            select.remove(selectedIndex);
+        }
+
+    });
+
+});
